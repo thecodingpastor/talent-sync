5d409e26dc07002c556d80620030d66ca2849487
Michael Ayeni <thecodingpastor@gmail.com>
2023-10-13 08:28:03 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tenetur
+      sunt dolorum possimus asperiores quaerat suscipit, quas ipsam hic
+      doloribus accusantium sed veritatis voluptatem dicta ullam nemo ducimus
+      doloremque nobis!
     </div>
   );
 }
